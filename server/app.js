aa91c733cc0a1708a114b39acbe4012b9745a768
UBC Student <ritikk7@students.cs.ubc.ca>
2023-07-24 18:15:26 -0700

    package change 2

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
-var itemsRouter = require('./routes/items');
+var itemsRouter = require('./routes/index');
 
 var app = express();
 
