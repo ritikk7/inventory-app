7fbf8231e93b6ac8c5943c3abfa3646eb3a85e1d
UBC Student <ritikk7@students.cs.ubc.ca>
2023-07-24 19:14:10 -0700

    db change 2

--- server/connectToDB.js
+++ server/connectToDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose').default;
 
 async function connectToDB() {
   try {
-    await mongoose.connect("DB_CONNECTION_STRING");
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
     console.log('Connected to the database');
   } catch (error) {
     console.error('Error connecting to the database', error);
